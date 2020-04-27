702013bb1fce69caab65d12c3071801e9db2a600
Geeoon Chung <geeoon13@gmail.com>
2020-04-27 18:59:56 -0500

    Added population and listing of an array

--- PythonSorting/PythonSorting.py
+++ PythonSorting/PythonSorting.py
@@ -1,1 +1,17 @@
-print ("Hello World")
+#Where are all the semicolons?
+
+import random
+
+def printElements(array):
+    for index in array:
+        print(index)
+
+def scrambleArray(array):
+    for index in range(0, len(array)):
+        array[index] = random.randrange(1, 100)
+
+numList = [None] * 100
+
+scrambleArray(numList)
+printElements(numList)
+
