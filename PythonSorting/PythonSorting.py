4a4ba7de9db3dc9531e43972b1de1ff98635e442
Geeoon Chung <geeoon13@gmail.com>
2020-04-27 22:01:23 -0500

    Added insertion sort

--- PythonSorting/PythonSorting.py
+++ PythonSorting/PythonSorting.py
@@ -8,7 +8,7 @@ def printElements(array):
 
 def scrambleArray(array):
     for index in range(0, len(array)):
-        array[index] = random.randrange(1, 100)
+        array[index] = random.randrange(1, 101)
 
 def swapElements(array, index1, index2):
     array[index1], array[index2] = array[index2], array[index1]
@@ -25,16 +25,28 @@ def bubbleSort(array):
                 swaps += 1
                 isArraySorted = False
                 swapElements(array, index-1, index)
-
+    printElements(array)
     print("\nComparisons: " + str(comparisons) + "\nSwaps: " + str(swaps) + "\n")
 
+def insertionSort(array):
+    comparisons = 0
+    swaps = 0
+    for i in range(1, len(array)):
+        j = i
+        comparisons += 1
+        while (j > 0) and (array[j-1] > array[j]):
+            comparisons += 1
+            swapElements(array, j-1, j)
+            swaps += 1
+            j -= 1
+    printElements(array)
+    print("\nComparisons: " + str(comparisons) + "\nSwaps: " + str(swaps) + "\n")
 
 numList = [None] * 100
 
 scrambleArray(numList)
 printElements(numList)
 
-print("\nSorting array...")
+print("\nSorting array...\n")
 
-bubbleSort(numList)
-printElements(numList)
+insertionSort(numList)
